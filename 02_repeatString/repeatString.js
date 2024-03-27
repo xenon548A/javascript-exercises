44d94234aebc1002e6031bbae003d84f3b0f8ef8
xenon548A <81672229+xenon548A@users.noreply.github.com>
2024-03-27 13:05:59 +0530

    complete 2-exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (num > 0){
+        for (let i = 1; i<=num; i++){
+            repeatedString += string;
+        };
+    } else if (num == 0) {
+        return "";
+    } else if(num < 0){
+        return "ERROR";
+    }
+    return repeatedString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
